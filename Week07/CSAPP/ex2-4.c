36da0605c760ba4d7eca2ebd4c3205d701245e91
apply <wonji052271@gmail.com>
2025-10-24 23:14:42 +0900

    solved

---
+++ Week07/CSAPP/ex2-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    void *brk = sbrk(0);
+    printf("Current program break: %p\n", brk);
+
+    FILE *fp = fopen("/proc/self/maps", "r");
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp)) {
+        if (strstr(line, "[heap]")) {
+            printf("Heap mapping: %s", line);
+            break;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
